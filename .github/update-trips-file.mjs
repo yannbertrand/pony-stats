9f0273ea7aed64f1d94922b2ac58d07f07894892
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-01-09 23:37:33 +0100

    commit changes

--- .github/update-trips-file.mjs
+++ .github/update-trips-file.mjs
@@ -4,10 +4,19 @@ const newTrips = JSON.parse(process.argv.at(2));
 
 const realNewTrips = await updateTripsFile(newTrips);
 
-const firstNewTripDate = new Date(realNewTrips.at(-1).startTime);
-const lastNewTripDate = new Date(realNewTrips.at(0).startTime);
-console.log(
-  `${
-    newTrips.length
-  } new trips saved from ${firstNewTripDate.toLocaleString()} to ${lastNewTripDate.toLocaleString()}`
-);
+if (realNewTrips.length >= 2) {
+  const firstNewTripDate = new Date(realNewTrips.at(-1).startTime);
+  const lastNewTripDate = new Date(realNewTrips.at(0).startTime);
+  console.log(
+    `${
+      newTrips.length
+    } new trips saved from ${firstNewTripDate.toLocaleString()} to ${lastNewTripDate.toLocaleString()}`
+  );
+} else if (realNewTrips.length === 1) {
+  const newTripDate = new Date(realNewTrips.at(0).startTime);
+  console.log(
+    `${newTrips.length} new trip saved from ${newTripDate.toLocaleString()}`
+  );
+} else {
+  console.log(`No new trips saved`);
+}
