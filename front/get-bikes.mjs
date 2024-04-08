809e1a3877ea4d6750f25d13575a66a35ce6941a
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-04-08 21:56:10 +0200

    Filter old bikes

--- front/get-bikes.mjs
+++ front/get-bikes.mjs
@@ -1,13 +1,18 @@
 export function getBikes(rawTrips) {
-  const bikes = rawTrips.reduce((acc, t) => {
-    if (!acc[t.bikeName]) {
-      acc[t.bikeName] = {
-        bikeName: t.bikeName,
-        firstTripDay: new Date(t.endTime),
-      };
-    }
-    return acc;
-  }, {});
+  const last6month = new Date();
+  last6month.setMonth(last6month.getMonth() - 6);
+
+  const bikes = rawTrips
+    .filter((trip) => new Date(trip.endTime) >= last6month)
+    .reduce((acc, t) => {
+      if (!acc[t.bikeName]) {
+        acc[t.bikeName] = {
+          bikeName: t.bikeName,
+          firstTripDay: new Date(t.endTime),
+        };
+      }
+      return acc;
+    }, {});
 
   return bikes;
 }
